cc6623c3ed93c455b0d09bf9ab48bff6d3d9c52c
Cristofer-Ortega <143019374+Cristofer-Ortega@users.noreply.github.com>
2024-11-20 22:28:29 -0600

    Add files via upload

---
+++ Ejemplo_Tareas/Ejemplo_Tareas/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        List<int> numeros = new List<int>();
+
+        Console.WriteLine("Introduce hasta 5 números:");
+
+        while (numeros.Count < 5)
+        {
+            string entrada = Console.ReadLine();
+
+            if (entrada.ToLower() == "fin")
+                break;
+
+            if (int.TryParse(entrada, out int numero))
+            {
+                numeros.Add(numero);
+            }
+            else
+            {
+                Console.WriteLine("Por favor, introduce un número válido o 'fin' para terminar.");
+            }
+        }
+
+        // Ordenar los números
+        var numerosOrdenados = await Task.Run(() => numeros.OrderBy(n => n).ToList());
+
+        // Sumar los números
+        var sumaTotal = await Task.Run(() => numerosOrdenados.Sum());
+
+        Console.WriteLine("Números ordenados: " + string.Join(", ", numerosOrdenados));
+        Console.WriteLine("Suma total: " + sumaTotal);
+    }
+}
+
